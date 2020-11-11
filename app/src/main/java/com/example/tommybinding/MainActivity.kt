31ffdb02dd25b67d854642e78e9028f22813d391
TrickyTommy <trickytommy4@gmail.com>
2020-11-11 15:53:06 +0700

    start

--- app/src/main/java/com/example/tommybinding/MainActivity.kt
+++ app/src/main/java/com/example/tommybinding/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.tommybinding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.CheckBox
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT =
+        3000L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed(
+            {
+                val i = Intent(this@MainActivity, HomeActivity::class.java)
+                startActivity(i)
+                finish()
+            }, SPLASH_TIME_OUT)
+        val checkBox = findViewById<CheckBox>(R.id.checkBox)
+        checkBox?.setOnCheckedChangeListener { buttonView, isChecked ->
+            val msg = "You have " + (if (isChecked) "checked" else "unchecked") + " this Check it Checkbox."
+            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+        }
     }
 }
