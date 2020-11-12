7fa4d5a9d501b117960968102c3b2bc7eaffe27e
TrickyTommy <trickytommy4@gmail.com>
2020-11-12 09:25:16 +0700

    wip

---
+++ app/src/main/java/com/example/tommybinding/Recycleactivity.kt
@@ -0,0 +1,11 @@
+package com.example.tommybinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Recycleactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycleactivity)
+    }
+}
